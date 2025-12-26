acb91d4f42b9b81661b0fb886e893d34db060d02
Francis de Moura Pereira <francisdemourapereira@gmail.com>
2025-12-26 15:28:38 -0300

    Enhance alterarStatus to manage game rental status
    Added status management for game items in dashboard.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,23 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
- 
+
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Rent';
+        gameClicado.dataset.status = '';
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Return';
+        gameClicado.dataset.status = 'rented';
     }
 }
+
+const links = document.querySelectorAll(".dashboard__items__item a");
+links.forEach(link => {
+    link.addEventListener("click", function(event){
+        event.preventDefault();
+    });
+});
